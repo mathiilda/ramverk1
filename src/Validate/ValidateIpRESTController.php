30f0bf2f9823e813fec3957076116ad744a72bb5
mathiilda <mathilda.tb17@gmail.com>
2020-11-04 19:31:10 +0100

    Fixed validation issue.

--- src/Validate/ValidateIpRESTController.php
+++ src/Validate/ValidateIpRESTController.php
@@ -17,7 +17,7 @@ class ValidateIpRESTController implements ContainerInjectableInterface
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS,"ip=" . $ipAddress);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, "ip=" . $ipAddress);
         $output = curl_exec($curl);
         curl_close($curl);
 
