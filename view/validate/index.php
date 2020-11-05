a3204682ee5b83950b894239b8c58971813dde8a
mathiilda <mathilda.tb17@gmail.com>
2020-11-05 12:56:16 +0100

    Updated the instructions of how to use the API.

--- view/validate/index.php
+++ view/validate/index.php
@@ -6,7 +6,8 @@
         <input type="submit" value="Skicka">
     </form>
     <?php if ($data["json"] == true) : ?>
-        <p>Svaret kommer att skrivas ut som JSON. För att hämta svaret anropas routen /api med ip-adressen du anger. Svaret returneras och skrivs ut på resultat-sidan.</p><br>
+        <br>
+        <p> <strong>Hur man använder API:et:</strong> För att hämta svaret anropa routen <code>/api</code>. Ip adressen anger du med nyckeln "ip" (i till exempel Postman). Svaret returneras som JSON.</p><br>
         <form action="api" method="POST">
             <input name="ip" type="hidden" value="2001:0db8:85a3:0000:0000:8a2e:0370:7334">
             <input type="submit" value="Testroute 1">
